cd301803048e29080784ba862ab16dbcdfe10bf8
Bruno Gallotte <brunogallotte1@gmail.com>
2024-02-04 11:16:59 -0300

    Feat: add e2e tests

---
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -0,0 +1,33 @@
+describe('template spec', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('shoud be able to navigate to the product page and ad it to the cart', () => {
+    cy.get('a[href^="/product"]').first().click()
+
+    cy.location('pathname').should('include', '/product')
+    cy.contains('Adicionar ao carrinho').click()
+
+    cy.contains('Cart (1)').should('exist')
+  })
+
+  it('should not count duplicated products on cart', () => {
+    cy.get('a[href^="/product"]').first().click()
+
+    cy.location('pathname').should('include', '/product')
+    cy.contains('Adicionar ao carrinho').click()
+    cy.contains('Adicionar ao carrinho').click()
+
+    cy.contains('Cart (1)').should('exist')
+  })
+
+  it('should be able to search for a product and add it to the cart', () => {
+    cy.searchByQuery('moletom')
+
+    cy.get('a[href^="/product"]').first().click()
+    cy.location('pathname').should('include', '/product')
+    cy.contains('Adicionar ao carrinho').click()
+    cy.contains('Cart (1)').should('exist')
+  })
+})
