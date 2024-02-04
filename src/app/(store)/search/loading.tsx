828c6ae4673d9888cfa1739a0ca7a45087263a92
Bruno Gallotte <brunogallotte1@gmail.com>
2024-02-04 11:45:36 -0300

    Fix: add suspense in search-form

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -2,6 +2,7 @@
 
 import { Skeleton } from '@/app/components/skeleton'
 import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
 
 export default function SearchLoading() {
   const searchParams = useSearchParams()
@@ -9,19 +10,21 @@ export default function SearchLoading() {
   const query = searchParams.get('q')
 
   return (
-    <div className="flex flex-col gap-4">
-      <p className="text-sm">
-        Resultados para: <span className="font-semibold">{query}</span>
-      </p>
+    <Suspense>
+      <div className="flex flex-col gap-4">
+        <p className="text-sm">
+          Resultados para: <span className="font-semibold">{query}</span>
+        </p>
 
-      <div className="grid grid-cols-3 gap-6">
-        <Skeleton className="h-[400px]" />
-        <Skeleton className="h-[400px]" />
-        <Skeleton className="h-[400px]" />
-        <Skeleton className="h-[400px]" />
-        <Skeleton className="h-[400px]" />
-        <Skeleton className="h-[400px]" />
+        <div className="grid grid-cols-3 gap-6">
+          <Skeleton className="h-[400px]" />
+          <Skeleton className="h-[400px]" />
+          <Skeleton className="h-[400px]" />
+          <Skeleton className="h-[400px]" />
+          <Skeleton className="h-[400px]" />
+          <Skeleton className="h-[400px]" />
+        </div>
       </div>
-    </div>
+    </Suspense>
   )
 }
